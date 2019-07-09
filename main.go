217a5944f473dfb56b65c118f3490efab94cf50c
batch <baaaakkrad@gmail.com>
2019-07-10 00:30:53 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+func signup(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("signup 関数実行")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("login 関数実行")
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+
+	log.Println("サーバー起動 : 8000 port で受信")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
