4f3aff57e2cb628ccdb96768a4a5556cd4ebef30
batch <baaaakkrad@gmail.com>
2019-07-17 21:29:53 +0900

    [update]signup password hash

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 
+	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -25,8 +27,69 @@ type Error struct {
 	Message string `json:"message"`
 }
 
+func errorInResponse(w http.ResponseWriter, status int, error Error) {
+    w.WriteHeader(status) // 400 とか 500 などの HTTP status コードが入る
+    json.NewEncoder(w).Encode(error)
+    return
+}
+
+func responseByJSON(w http.ResponseWriter, data interface{}) {
+	json.NewEncoder(w).Encode(data)
+	return
+}
+
 func signup(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("signup 関数実行")
+		fmt.Println("signup 関数実行")
+		var user User
+		var error Error
+
+		fmt.Println(r.Body)
+
+		json.NewDecoder(r.Body).Decode(&user)
+
+		if user.Email == "" {
+			error.Message = "Emailは必須です"
+			errorInResponse(w, http.StatusBadRequest, error)
+			return
+		}
+
+		if user.Password == "" {
+			error.Message = "パスワードは必須です"
+			errorInResponse(w, http.StatusBadRequest, error)
+			return
+		}
+
+		//fmt.Println(user)
+
+		fmt.Println("---------------------")
+		//spew.Dump(user)
+
+		hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("パスワード：", user.Password)
+		fmt.Println("ハッシュ化されたパスワード", hash)
+
+		user.Password = string(hash)
+		fmt.Println("コンバート後のパスワード：", user.Password)
+
+		sql_query := "INSERT INTO USERS(EMAIL, PASSWORD) VALUES($1, $2) RETURNING ID"
+
+		err = db.QueryRow(sql_query, user.Email, user.Password).Scan(&user.ID)
+
+		if err != nil {
+			error.Message = "サーバエラー"
+			errorInResponse(w, http.StatusInternalServerError, error)
+			return
+		}
+
+		user.Password = ""
+		w.Header().Set("Content-Type", "application/json")
+
+		responseByJSON(w, user)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -39,8 +102,10 @@ func execDB(db *sql.DB, q string) {
 	}
 }
 
-func main() {
+var db *sql.DB
 
+func main() {
+/*
 	db, err := sql.Open("sqlite3", "./sample.sqlite3")
 	if err != nil {
 		log.Fatal(err)
@@ -51,8 +116,8 @@ func main() {
 	`
 
 	execDB(db, q)
-
 	db.Close()
+	*/
 
 	router := mux.NewRouter()
 
